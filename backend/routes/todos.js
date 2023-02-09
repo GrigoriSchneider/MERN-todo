54993d5018848c57ae9476fb9de2f181c7fed9d7
unknown <grigorischneider@gmail.com>
2023-02-09 18:44:26 +0100

    Joi Data Validation

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,9 +1,23 @@
 const { Todo } = require("../models/todo");
 const express = require("express");
+const Joi = require("joi");
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(200).required(),
+    author: Joi.string().min(3).max(30),
+    uid: Joi.string(),
+    isComplete: Joi.boolean(),
+    date: Joi.date(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  // bad request client error
+  if (error) return res.status(400).send(error.details[0].message);
+
   const { name, author, isComplete, date, uid } = req.body;
 
   // constructor
@@ -19,6 +33,7 @@ router.post("/", async (req, res) => {
     todo = await todo.save();
     res.send(todo);
   } catch (error) {
+    // 500 server error
     res.status(500).send(error.message);
     console.log(error.message);
   }
