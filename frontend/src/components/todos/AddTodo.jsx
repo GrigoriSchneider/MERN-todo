90d6fa7f32dce7359b3ae0b022b165ad9e99c2e3
unknown <grigorischneider@gmail.com>
2023-02-15 15:35:42 +0100

    create todos

--- frontend/src/components/todos/AddTodo.jsx
+++ frontend/src/components/todos/AddTodo.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -6,10 +7,29 @@ import SendIcon from "@mui/icons-material/Send";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 
+import { addTodo } from "../../store/actions/todoActions";
+
 const AddTodo = () => {
+  const dispatch = useDispatch();
+
+  const [todo, setTodo] = useState({
+    name: "",
+    isComplete: false,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(addTodo(todo));
+    setTodo({
+      name: "",
+      isComplete: false,
+    });
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Box
           sx={{
             m: 2,
@@ -32,12 +52,17 @@ const AddTodo = () => {
               autoFocus
               fullWidth
               sx={{ mt: 3 }}
+              value={todo.name}
+              onChange={(e) =>
+                setTodo({ ...todo, name: e.target.value, date: new Date() })
+              }
             />
             <Button
               color="primary"
               variant="contained"
               sx={{ mt: 3, ml: 1 }}
               size="large"
+              type="submit"
             >
               <SendIcon />
             </Button>
