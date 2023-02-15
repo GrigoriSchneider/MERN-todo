0e622ed4e3cdeee673fdbb1d7c6f3dc6cc98f228
unknown <grigorischneider@gmail.com>
2023-02-15 13:53:34 +0100

    redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { Provider } from "react-redux";
+
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+
+import thunk from "redux-thunk";
+
+import rootReducer from "./store/reducers/rootReducer";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
